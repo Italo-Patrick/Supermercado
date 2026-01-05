b7dcdb0382a01937bbd90a33fe5a5903b18a35d7
Italo Patrick <italopatrick247@gmail.com>
2026-01-05 13:35:40 -0300

    Atualização do menu hamburger, para que ele fexhe com o click fora

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
-console.log('menu.js carregado');
-
 const menuToggle =document.querySelector('.menu-toggle');
 const navLinks = document.querySelector('.nav-links')
 
 menuToggle.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
+
+document.addEventListener('click', (e)=>
+{   const clicouNoMenu = navLinks.contains(e.target);
+    const clicouNobotao = menuToggle.contains(e.target);
+
+    if(!clicouNoMenu && !clicouNobotao) {
+        navLinks.classList.remove('active');
+    }
+})
